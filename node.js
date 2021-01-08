d58c4e950ef8bd14dd1a379a2768f53326a59c39
softwaree40 <softwaree40@gmail.com>
2021-01-08 15:05:02 -0500

    using filesystem to read dir

--- node.js
+++ node.js
@@ -3,3 +3,13 @@ let totalMemory = os.totalmem()
 let freeMemory = os.freemem
 console.log(`This is Total Memory: ${totalMemory}`)
 console.log(`This is Free Memory: ${freeMemory}`)
+// create files system with node
+
+const fs = require("fs")
+fs.readdir("./",(errors,files)=>{
+   if(errors){
+     console.log(errors)
+   }else{
+       console.log("I am files",files)
+   }
+})
